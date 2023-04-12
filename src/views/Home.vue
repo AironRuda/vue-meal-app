5474c6d7872162463105f7814a5869bea47f16dc
AironRuda <airon.ruda@udea.edu.co>
2023-04-11 20:30:19 -0500

    feat: search input & letter selector & navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
-    <pre>{{ meals }}</pre>
+  <div class="flex p-8 flex-col items-center justify-center">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for meals"
+    />
+    <div class="flex gap-2 mt-2">
+      <router-link
+        :to="{
+          name: 'byletter',
+          params: { letter },
+        }"
+        :key="letter"
+        v-for="letter of letters"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import store from '../store';
+import { computed } from "vue";
+import store from "../store";
 
-const meals = computed(() => store.state.meals)
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>
