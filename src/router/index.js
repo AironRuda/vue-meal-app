09ab00ff72966663d0a899b016faf18a2f99d330
AironRuda <airon.ruda@udea.edu.co>
2023-04-12 00:02:20 -0500

    feat: defeat layout & navbar as component

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import DefaultLayout from "../components/DefaultLayout.vue";
 import Home from "../views/Home.vue";
 import MealsByIngredient from "../views/MealsByIngredient.vue";
 import MealsByLetter from "../views/MealsByLetter.vue";
@@ -8,28 +9,34 @@ import MealList from "../views/MealList.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/by-name/:name?",
-    name: "byName",
-    component: MealsByName,
-  },
-  {
-    path: "/by-letter/:letter?",
-    name: "byLetter",
-    component: MealsByLetter,
-  },
-  {
-    path: "/by-ingredient/:ingredient?",
-    name: "byIngredient",
-    component: MealsByIngredient,
-  },
-  {
-    path: "/letter/:letter",
-    name: "byletter",
-    component: MealList,
+    component: DefaultLayout,
+    children: [
+      {
+        path: "/",
+        name: "home",
+        component: Home,
+      },
+      {
+        path: "/by-name/:name?",
+        name: "byName",
+        component: MealsByName,
+      },
+      {
+        path: "/by-letter/:letter?",
+        name: "byLetter",
+        component: MealsByLetter,
+      },
+      {
+        path: "/by-ingredient/:ingredient?",
+        name: "byIngredient",
+        component: MealsByIngredient,
+      },
+      {
+        path: "/letter/:letter",
+        name: "byletter",
+        component: MealList,
+      },
+    ],
   },
 ];
 
