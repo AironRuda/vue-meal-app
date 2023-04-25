dd89545743089a010b84a305904d4d9d889f2061
AironRuda <airon.ruda@udea.edu.co>
2023-04-25 18:04:47 -0500

    feat: reusable youtube button component

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <a
+    :href="href"
+    target="_blanck"
+    class="p-3 rounded border-2 border-red-600 hover:bg-red-500 hover:text-white transition-colors"
+    >YouTube</a
+  >
+</template>
+<script setup>
+const props = defineProps({
+  href: String,
+});
+</script>
