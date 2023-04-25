c81a199cf5cf81adb25277ae0ada40c7276fb90f
AironRuda <airon.ruda@udea.edu.co>
2023-04-25 17:10:35 -0500

    feat: detail meal on router

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import MealsByIngredient from "../views/MealsByIngredient.vue";
 import MealsByLetter from "../views/MealsByLetter.vue";
 import MealsByName from "../views/MealsByName.vue";
 import MealList from "../views/MealList.vue";
+import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
@@ -36,6 +37,11 @@ const routes = [
         path: "/letter/:letter",
         name: "byletter",
         component: MealList,
+      },
+      {
+        path: "/meal/:id",
+        name: "mealDetails",
+        component: MealDetails,
       },
     ],
   },
