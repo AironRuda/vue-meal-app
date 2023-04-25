bf4aa73f6b7dfb067a0f0478e50fef340fdf406e
AironRuda <airon.ruda@udea.edu.co>
2023-04-25 17:38:48 -0500

    feat: detail meal view

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    <!-- <img src="" alt="" /> -->
-    <pre>{{ meal }}</pre>
+  <div class="w-[800px] mx-auto p-8">
+    <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" />
+    <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
+      <div>
+        <strong class="font-bold"> Category: </strong>
+        {{ meal.strCategory }}
+      </div>
+      <div>
+        <strong class="font-bold"> Area: </strong>
+        {{ meal.strArea }}
+      </div>
+      <div>
+        <strong class="font-bold"> Tags: </strong>
+        {{ meal.strTags }}
+      </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
+        <ul>
+          <template v-for="(element, index) of new Array(20)">
+            <li v-if="meal[`strIngredient${index + 1}`]">
+              {{ index + 1 }}. {{ meal[`strIngredient${index + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Measures</h2>
+        <ul>
+          <template v-for="(element, index) of new Array(20)">
+            <li v-if="meal[`strMeasure${index + 1}`]">
+              {{ index + 1 }}. {{ meal[`strMeasure${index + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
