1f99ccb51ae7a5ea74853b75739bb9ab3b3d4dcc
AironRuda <airon.ruda@udea.edu.co>
2023-04-25 17:10:49 -0500

    feat: detail meal component

---
+++ src/views/MealDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- <img src="" alt="" /> -->
+    <pre>{{ meal }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import axiosClient from "../axiosClient";
+
+const route = useRoute();
+const meal = ref({});
+
+onMounted(() => {
+  axiosClient.get(`lookup.php?i=${route.params.id}`).then(({ data }) => {
+    meal.value = data.meals[0] || {};
+  });
+});
+</script>
