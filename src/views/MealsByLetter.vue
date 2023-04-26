bb43922f762e4e14f02c889d1fcc44ebab6ff86f
AironRuda <airon.ruda@udea.edu.co>
2023-04-26 12:40:36 -0500

    feat: meals byletter functional

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex justify-center gap-2 mt-2">
     <router-link
+      class="hover:font-bold"
       :to="{
         name: 'byLetter',
         params: { letter },
@@ -11,19 +12,24 @@
       {{ letter }}
     </router-link>
   </div>
-  <div>
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
 </template>
 
 <script setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, watch } from "vue";
 import store from "../store";
 import { useRoute } from "vue-router";
+import MealItem from "../components/MealItem.vue";
 
 const route = useRoute();
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const meals = computed(() => store.state.mealsByLetter);
+
+watch(route, () => {
+  store.dispatch("searchMealsByLetter", route.params.letter);
+});
 
 onMounted(() => {
   store.dispatch("searchMealsByLetter", route.params.letter);
