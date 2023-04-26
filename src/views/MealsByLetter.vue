3924a1b1ee6040e4b3eabc4cc242a04eff37b795
AironRuda <airon.ruda@udea.edu.co>
2023-04-26 11:18:56 -0500

    feat: show meals by letter

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -2,17 +2,30 @@
   <div class="flex justify-center gap-2 mt-2">
     <router-link
       :to="{
-        name: 'byletter',
+        name: 'byLetter',
         params: { letter },
       }"
-      :key="letter"
       v-for="letter of letters"
+      :key="letter"
     >
       {{ letter }}
     </router-link>
   </div>
+  <div>
+    <pre>{{ meals }}</pre>
+  </div>
 </template>
 
 <script setup>
+import { computed, onMounted } from "vue";
+import store from "../store";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const meals = computed(() => store.state.mealsByLetter);
+
+onMounted(() => {
+  store.dispatch("searchMealsByLetter", route.params.letter);
+});
 </script>
